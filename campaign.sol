b660d5c4033cd3766352b410f72d657f738a5716
pllearns <pllearns@gmail.com>
2018-01-26 16:14:53 -0800

    add campaign contract after testing complete

---
+++ campaign.sol
@@ -0,0 +1,58 @@
+pragma solidity ^0.4.17;
+
+contract Campaign {
+    
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvals;
+    }
+    
+    Request[] public requests;
+    address public manager;
+    uint public minimumContribution;
+    mapping(address => bool) public approvers;
+    
+    
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+    
+    function Campaign(uint minimum) public {
+        manager = msg.sender;
+        minimum = minimumContribution;
+    }
+    
+    function contribute() public payable {
+        require(msg.value > minimumContribution);
+        approvers[msg.sender] = true;
+    }
+    
+    function createRequest(string description, uint value, address recipient)
+        public restricted {
+        require(approvers[msg.sender]);
+        Request memory newRequest = Request({
+            description: description,
+            value: value,
+            recipient: recipient,
+            complete: false,
+            approvalCount: 0
+        });
+        
+        requests.push(newRequest);
+    }
+    
+    function approveRequest(uint index) public {
+        Request storage request = requests[index]; 
+        
+        require(approvers[msg.sender]);
+        require(!request.approvals[msg.sender]);
+        
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+    }
+}
